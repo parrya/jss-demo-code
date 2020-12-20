b4ea434023e45ad1ee446100676c44108c723e97
Andy Parry <andy.parry@hbf.com.au>
2020-12-20 15:02:26 +0800

    Created separate metadata project and moved into Feature solution folder

---
+++ sitecore/src/Feature/Metadata/website/JSSDemo.Feature.Metadata/PipelineProcessors/ItemContext.cs
@@ -0,0 +1,22 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.LayoutService.ItemRendering.Pipelines.GetLayoutServiceContext;
+using System.Collections.Generic;
+using Sitecore;
+using Sitecore.Links;
+
+namespace JSSDemo.Feature.Metadata.PipelineProcessors
+{
+    public class ItemContext : IGetLayoutServiceContextProcessor
+    {
+        public const string Key = "itemUrl";
+
+        public void Process(GetLayoutServiceContextArgs args)
+        {
+            Assert.ArgumentNotNull((object)args, nameof(args));
+            var options = LinkManager.GetDefaultUrlBuilderOptions();
+            options.AlwaysIncludeServerUrl = true;
+            string link = LinkManager.GetItemUrl(Context.Item, options);
+            args.ContextData.Add(Key, link);
+        }
+    }
+}
